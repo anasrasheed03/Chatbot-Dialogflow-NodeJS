91c5a0e41fbc0f4dd9eecbfec7e57ed254a76016
Syed Anas <syed-anas@hotmail.my>
2019-05-18 15:12:36 -0400

    First commit

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const messageRouter = new MessageRouter({
   operatorRoom: io.of('/operator')
 });
 
-// Serve static html files for the customer and operator client
+// Serve static html files for the customer and operator clients
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/static/customer.html`);
 });
